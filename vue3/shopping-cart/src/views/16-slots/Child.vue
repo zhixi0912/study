5ddd6a9df6222394a30af7fbb7cee6033e5c455d
zhixi0912 <985840977@qq.com>
2022-10-12 16:58:19 +0800

    插槽

---
+++ vue3/shopping-cart/src/views/16-slots/Child.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <slot :mark="mark" mark2="--->"></slot>
+        <slot name="line2"></slot>
+    </div>
+</template>
+
+<script setup>
+    //局部注册组件
+import {ref} from 'vue'
+const mark = ref('!!!')
+</script>
+
+<style lang="css">
+
+</style>
---
+++ vue3/shopping-cart/src/views/16-slots/Demo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <Child>
+            <!-- <template #default="data"> -->
+            <template v-slot:default="data">
+                <div>{{str}} {{data.mark2}} world {{data.mark}}</div>
+            </template>
+            <template v-slot:line2>
+                <div>{{str}} slot</div>
+            </template>
+        </Child>
+    </div>
+</template>
+
+<script setup>
+ import { ref  } from 'vue';
+import Child from './Child.vue'
+const str = ref('hello')
+</script>
+
+<style lang="css">
+
+</style>
