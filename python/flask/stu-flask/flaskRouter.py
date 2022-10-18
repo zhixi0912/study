f3b1e2395e4295e1164287e8496e71481a1d64d9
zhixi0912 <985840977@qq.com>
2022-10-18 16:31:27 +0800

    python学习基础

--- python/flask/stu-flask/flaskRouter.py
+++ python/flask/stu-flask/flaskRouter.py
@@ -13,9 +13,12 @@ app = Flask(__name__)
 # def hi():
 #     return 'hi hi'
 
-
+#string 接收任何不包含斜杠的文本
+#int 接收正整数
+#float 接收正浮点数 (2.2)
+#path 接收包含斜杠的文本
 #路由传参 变量规则
-@app.route('/user/<id>')
+@app.route('/user/<float:id>')
 def index(id):
     if id == '1':
         return 'python'
---
+++ python/flask/stu-flask/自定义转换器.py
@@ -0,0 +1,8 @@
+# <>提取参数
+# <int:id> int 转换器
+
+#自定义转换器
+
+from werkzeug.routing import BaseConverter
+from flask import Flask
+app = Flask(__name__)
