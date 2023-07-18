de62cc6c8568cbd0e772d9324c0e7ac7f74214f8
zhixi0912 <985840977@qq.com>
2023-07-18 19:59:11 +0800

    添加登录页

--- vue3/es6-ts-demo/src/router/index.ts
+++ vue3/es6-ts-demo/src/router/index.ts
@@ -7,12 +7,12 @@ export const constantRoutes = [
     component: () => import('../views/home/index.vue')
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
     // this generates a separate chunk (About.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import('../views/AboutView.vue')
+    component: () => import('../views/login/index.vue')
   }
 ]
 
