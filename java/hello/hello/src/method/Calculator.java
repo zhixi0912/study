7ecdfb4560c626a1b3add6e8a3456c4018eef591
jiangchuan <985840977@qq.com>
2023-08-08 09:41:46 +0800

    计算器方法

--- java/hello/hello/src/method/Calculator.java
+++ java/hello/hello/src/method/Calculator.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Calculator {
     /**
      * 计算器功能，要求实现加减乘除功能，
-     * 并且能够循环接收新的数据，通过用户交互实现
+     * 并且能够循环接收新的数据，
+     * 通过用户交互实现
      */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -18,7 +19,7 @@ public class Calculator {
         } else {
             System.out.println("非数字");
             if (preams == '+') {
-                System.out.println("这是加号");
+//                System.out.println("这是加号");
             }
         }
 
