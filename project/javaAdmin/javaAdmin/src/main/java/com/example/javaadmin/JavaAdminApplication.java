b2b83e0b73d9bc5a344caac1c8c8751274d47595
jiangchuan <985840977@qq.com>
2023-08-23 13:57:57 +0800

    添加java后端

---
+++ project/javaAdmin/javaAdmin/src/main/java/com/example/javaadmin/JavaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaAdminApplication.class, args);
+    }
+
+}
