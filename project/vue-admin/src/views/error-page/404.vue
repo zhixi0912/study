7d9739b7e5a12d14c3e8a3cc52cada66f95778aa
jiangchuan <985840977@qq.com>
2023-09-06 09:18:01 +0800

    更新返回按键

--- project/vue-admin/src/views/error-page/404.vue
+++ project/vue-admin/src/views/error-page/404.vue
@@ -52,7 +52,7 @@ function message() {
           Please check that the URL you entered is correct, or click the button
           below to return to the homepage.
         </div>
-        <a href="" class="bullshit__return-home">Back to home</a>
+        <a href="" class="bullshit__return-home">返回</a>
       </div>
     </div>
   </div>
--- project/vue-admin/src/views/login/index.vue
+++ project/vue-admin/src/views/login/index.vue
@@ -7,6 +7,6 @@
 </script>
 
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
