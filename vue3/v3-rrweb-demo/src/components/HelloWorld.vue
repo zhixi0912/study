f2157753dce29ba8e742dbc9aac1000d99b00351
jiangchuan <985840977@qq.com>
2023-10-12 14:38:04 +0800

    更新组件内容

--- vue3/v3-rrweb-demo/src/components/HelloWorld.vue
+++ vue3/v3-rrweb-demo/src/components/HelloWorld.vue
@@ -3,20 +3,11 @@ import { ref, onMounted } from 'vue'
 
 
 
-const dialogVisible = ref(false)
+
 defineProps<{ msg: string }>()
 
 const count = ref(0)
-const loading = ref(true)
-const handleClose = (done: () => void) => {
-  ElMessageBox.confirm('Are you sure to close this dialog?')
-    .then(() => {
-      done()
-    })
-    .catch(() => {
-      // catch error
-    })
-}
+
 
 
 
@@ -53,33 +44,11 @@ onMounted(() => {
     in your IDE for a better DX
   </p>
   <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-  <el-dialog
-    v-model="dialogVisible"
-    title="录屏回放"
-    width="30%"
-    :before-close="handleClose"
-    center
-  >
-    <div class="vido-box" v-loading="loading">
-      <span>This is a message</span>
-    
-    </div>
-    <template #footer>
-      <span class="dialog-footer">
-        <el-button @click="dialogVisible = false">Cancel</el-button>
-        <el-button type="primary" @click="dialogVisible = false">
-          Confirm
-        </el-button>
-      </span>
-    </template>
-  </el-dialog>
+
 </template>
 
 <style scoped>
 .read-the-docs {
   color: #888;
-}
-.vido-box {
-  min-height: 300px;
 }
 </style>
--- vue3/v3-rrweb-demo/src/components/Record.vue
+++ vue3/v3-rrweb-demo/src/components/Record.vue
@@ -10,9 +10,30 @@
       <el-button type="primary" :icon="VideoCamera" @click="dialogVisible = true" >播放</el-button>
     </el-col>
 </el-row>
+<el-dialog
+    v-model="dialogVisible"
+    title="录屏回放"
+    width="30%"
+    :before-close="handleClose"
+    center
+>
+    <div class="vido-box" v-loading="loading">
+      <span>This is a message</span>
+
+    </div>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">
+          Confirm
+        </el-button>
+      </span>
+    </template>
+</el-dialog>
 </template>
 <script lang="ts" setup>
 import RecordRTC from 'recordrtc';
+import { ref, onMounted } from 'vue'
 import { ElMessageBox, ElMessage } from 'element-plus'
 import {
     VideoPlay,
@@ -34,4 +55,21 @@ const stopRecord = () => {
     })
 
 }
+
+const dialogVisible = ref(false)
+const loading = ref(true)
+const handleClose = (done: () => void) => {
+    ElMessageBox.confirm('Are you sure to close this dialog?')
+    .then(() => {
+        done()
+    })
+    .catch(() => {
+        // catch error
+    })
+}
 </script>
+<style scoped>
+.vido-box {
+  min-height: 300px;
+}
+</style>
