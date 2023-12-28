8b46fa48284e487c0098974c7b89f98f391293e8
jiangchuan <985840977@qq.com>
2023-12-28 09:06:48 +0800

    msg

--- vue3/es6-ts-demo/src/components/HelloWorld.vue
+++ vue3/es6-ts-demo/src/components/HelloWorld.vue
@@ -9,7 +9,6 @@ props.msg = 'hello'
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
     </h3>
